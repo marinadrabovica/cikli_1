c9f10d108cb9b6668d386b077d8152b9e39a8f9d
Marina Draboviča <102215828+marinadrabovica@users.noreply.github.com>
2022-04-28 06:30:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    for (int y = 1; y <= 2; y++) {
+            for (int x = 1; x <= 3; x++) {
+                System.out.println(x + ", " + y);
+            }
+      }
+  }
+}
